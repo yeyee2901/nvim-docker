a9f3d61c0a6b23bd979ce4edf09c5471f5a3f3bb
Gabriel Sebastian <gabriel777sh@gmail.com>
2022-10-19 20:52:16 +0700

    FIX: colorscheme not loading

--- nvim/lua/user/theme/init.lua
+++ nvim/lua/user/theme/init.lua
@@ -1,8 +1,2 @@
-local theme_ok, _ = pcall(require, 'onedarkpro')
-local lualine_ok, _ = pcall(require, 'lualine')
-local nvim_tree_ok, _ = pcall(require, 'nvim-tree')
-
-if theme_ok and lualine_ok and nvim_tree_ok then
-  require 'user.theme.color'
-  require 'user.theme.lualine'
-end
+require 'user.theme.color'
+require 'user.theme.lualine'
